b19c703f81169e47332da74d31096c2559499c68
jidantou <wang3bo3yang3@126.com>
2022-03-12 22:10:50 +0800

    change in 03/12

--- pack.cpp
+++ pack.cpp
@@ -5,13 +5,23 @@
 using namespace jidantou;
 
 using std::string;
+using std::fstream;
+using std::ios;
 
-bool PackFile()
+// pack file into several files and entrypt
+bool PackFile(string fileName, string path, int size, string key)
 {
-    ;
+    fstream * pfile = new fstream(fileName, ios::binary | ios::in | ios::_Nocreate);
+
+    pfile->close();
+    delete pfile;
 }
 
-bool Unpackfiles()
-{
-    ;
-}
+// pack file into several files with no entrypting
+bool PackFile(string fileName, string path, int size);
+
+// unpack files and restore with entrypting
+bool UnpackFiles(string fileName, string path, int size, string key);
+
+// unpack files and restore with no entrypting
+bool UnpackFiles(string fileName, string path, int size);
--- pack.h
+++ pack.h
@@ -1,9 +1,21 @@
 #ifndef PACK_H
 #define PACK_H
 
+#include <iostream>
+using std::string;
+
 namespace jidantou
 {
-    bool PackFile();
-    bool Unpackfiles();
+    // pack file into several files and entrypt
+    bool PackFile(string fileName, string path, int size, string key);
+
+    // pack file into several files with no entrypting
+    bool PackFile(string fileName, string path, int size);
+
+    // unpack files and restore with entrypting
+    bool UnpackFiles(string fileName, string path, int size, string key);
+
+    // unpack files and restore with no entrypting
+    bool UnpackFiles(string fileName, string path, int size);
 }
 #endif
