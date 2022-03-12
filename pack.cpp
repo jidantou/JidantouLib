446401dc0805d0048e8f36b9a738d3aa10e33d26
jidantou <wang3bo3yang3@126.com>
2022-03-12 21:49:55 +0800

    save0

--- pack.cpp
+++ pack.cpp
@@ -1,3 +1,17 @@
 #include "pack.h"
+#include <iostream>
+#include <fstream>
 
 using namespace jidantou;
+
+using std::string;
+
+bool PackFile()
+{
+    ;
+}
+
+bool Unpackfiles()
+{
+    ;
+}
--- pack.h
+++ pack.h
@@ -3,34 +3,7 @@
 
 namespace jidantou
 {
-    class pack
-    {
-    private:
-    public:
-        pack();
-        virtual bool packfile() = 0;
-        virtual bool unpackfiles() = 0;
-        virtual ~pack() = 0;
-    };
-
-    class _packing : public pack
-    {
-    private:
-        /* data */
-    public:
-        _packing(/* args */);
-        ~_packing();
-    };
-
-    class _unpacking : public pack
-    {
-    private:
-        /* data */
-    public:
-        _unpacking(/* args */);
-        ~_unpacking();
-    };
-
-    extern void setmode();
+    bool PackFile();
+    bool Unpackfiles();
 }
 #endif
